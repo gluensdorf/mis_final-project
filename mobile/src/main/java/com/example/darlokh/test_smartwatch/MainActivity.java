d0ebd026c06cbc1e339f48c237705bc517efbbab
g.luensdorf <g.luensdorf@gmail.com>
2018-06-15 15:41:12 +0200

    shrink onCreate

--- mobile/src/main/java/com/example/darlokh/test_smartwatch/MainActivity.java
+++ mobile/src/main/java/com/example/darlokh/test_smartwatch/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.darlokh.test_smartwatch;
 
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
 
         setContentView(R.layout.activity_main);
 
