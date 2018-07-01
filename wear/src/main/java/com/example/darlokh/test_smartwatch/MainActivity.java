a7783afb9745dca48839800ebec8aa0575fad3ba
g.luensdorf <g.luensdorf@gmail.com>
2018-07-01 11:33:59 +0200

    merge wear and mobile projects

--- wear/src/main/java/com/example/darlokh/test_smartwatch/MainActivity.java
+++ wear/src/main/java/com/example/darlokh/test_smartwatch/MainActivity.java
@@ -1,21 +1,105 @@
 package com.example.darlokh.test_smartwatch;
 
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+import java.util.EventListener;
+import java.util.List;
 
-    private TextView mTextView;
+public class MainActivity extends WearableActivity implements SensorEventListener {
+
+    public SensorManager mSensorManager;
+    public Sensor mGravitySensor;
+    public Sensor mMagnetometer;
+    public Sensor mAccelerometer;
+
+    public float[] mGravity;
+    public float[] mMagneticRotationData;
+    private float azimutInDegrees;
+
+    private int mAzimuth = 0;
+
+    private MyView circleMyView;//= new MyView(this.getApplicationContext());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        circleMyView = new MyView(this.getApplicationContext());
+        setContentView(circleMyView);
 
-        // Enables Always-on
-        setAmbientEnabled();
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mGravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume", "onResume: onResume");
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("onPause", "onPause: onPause");
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    //as little action as possible within this function
+    public void onSensorChanged(SensorEvent event) {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            mMagneticRotationData = event.values;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
+            mGravity = event.values;
+        }
+
+        if (mGravity != null && mMagneticRotationData != null) {
+            sensorAction();
+        }
+    }
+
+    public void sensorAction() {
+        float R[] = new float[9];
+        float I[] = new float[9];
+
+        boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mMagneticRotationData);
+        if (success) {
+            float orientation[] = new float[3];
+            SensorManager.getOrientation(R, orientation);
+
+            mAzimuth = (int) (Math.toDegrees( SensorManager.getOrientation( R, orientation) [0]) + 360) %360;
+            Log.d("data", "sensorAction: AZi" + " " + mAzimuth);
+
+            circleMyView.setDegrees(mAzimuth);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("STOP", "onStop: STOP");
     }
 }
---
+++ wear/src/main/java/com/example/darlokh/test_smartwatch/MyView.java
@@ -0,0 +1,47 @@
+package com.example.darlokh.test_smartwatch;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyView extends View
+{
+    Paint paint = new Paint();
+    Canvas canvas;
+    public int x,y,rad;
+    public float turnDegrees;
+
+    public MyView(Context context)
+    {
+        super(context);
+//        paint = ;
+//        canvas = new Canvas();
+        x = 50;
+        y = 100;
+        rad = 20;
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+//        canvas.drawPaint(paint);
+        paint.setColor(Color.parseColor("#CD5C5C"));
+
+    }
+
+    @Override
+    protected void onDraw(Canvas blaCanvas) {
+        super.onDraw(blaCanvas);
+        canvas = blaCanvas;
+        canvas.rotate(turnDegrees, this.getWidth()/2, this.getHeight()/2);
+        canvas.drawCircle(x, y, rad, paint);
+        invalidate();
+    }
+
+
+    void setDegrees(float newDegrees) {
+//        canvas.rotate(newDegrees);
+        //both newDegrees and turnDegrees are working fine
+        turnDegrees = newDegrees;
+    }
+}
