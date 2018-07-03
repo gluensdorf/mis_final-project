7f26d2f8586ace9dbe666383054ee200ce0ff23f
g.luensdorf <g.luensdorf@gmail.com>
2018-07-03 10:32:06 +0200

    add landmark-class

---
+++ mobile/src/main/java/com/example/darlokh/test_smartwatch/landmark.java
@@ -0,0 +1,23 @@
+package com.example.darlokh.test_smartwatch;
+
+public class landmark {
+    public int x;
+    public int y;
+    public String tag;
+
+    void landmark(int newX, int newY, String newTag){
+        x = newX;
+        y = newY;
+        tag = newTag;
+    }
+//
+//    //get-Functions
+//    int getX(){return x;}
+//    int getY(){return y;}
+//    String getTag(){return tag;}
+//
+//    //set-Functions
+//    void setX(int newX){x = newX;}
+//    void setY(int newY){y = newY;}
+//    void setTag(String newTag){tag = newTag;}
+}
